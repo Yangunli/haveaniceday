0c77abb8f32325da43d21f09ee074eb0aa091cd2
Yangunli <yangun410@gmail.com>
2022-09-09 15:21:16 +0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,24 +7,24 @@ const Footer = () => {
     <div className="footer">
       <div className="ref">
         <div className="footer-c">2022 © Yangunli</div>
+        <div className="footer-icons">
+          <a href="https://twitter.com/yanyangirl_tw">
+            <img src={twitter} alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/wei-yi-li-898baa234/">
+            <img src={linkedin} alt="" />
+          </a>
+
+          <a href="https://github.com/Yangunli">
+            <img src={github} alt="" />
+          </a>
+        </div>
         <div className="footer-link">
           <div className="footer-a">
             <small>
               Illustrations by
               <a href="https://storyset.com/people"> Storyset </a>
             </small>
-          </div>
-          <div className="footer-icons">
-            <a href="https://twitter.com/yanyangirl_tw">
-              <img src={twitter} alt="" />
-            </a>
-            <a href="https://www.linkedin.com/in/wei-yi-li-898baa234/">
-              <img src={linkedin} alt="" />
-            </a>
-
-            <a href="https://github.com/Yangunli">
-              <img src={github} alt="" />
-            </a>
           </div>
         </div>
       </div>
